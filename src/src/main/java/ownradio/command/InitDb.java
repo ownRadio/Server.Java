f9213d1463687c488801dca828422bbef2b5de29
Tanat <researcher2286@gmail.com>
2016-10-19 11:27:54 +0600

    Настроить инициализатор БД тестовыми данными
    Добавление в БД тестовых данных будет происходить при запуске приложения в режиме dev

--- src/src/main/java/ownradio/command/InitDb.java
+++ src/src/main/java/ownradio/command/InitDb.java
@@ -1,11 +1,15 @@
 package ownradio.command;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import ownradio.domain.User;
 import ownradio.service.UserService;
 
+@Slf4j
+@Profile("dev")
 @Component
 public class InitDb implements CommandLineRunner {
 	@Autowired
@@ -13,8 +17,7 @@ public class InitDb implements CommandLineRunner {
 
 	@Override
 	public void run(String... strings) throws Exception {
-		User user = new User();
-		userService.save(user);
-		System.out.println(user.getId());
+		User user = userService.save(new User());
+		log.debug("User id: {}", user.getId());
 	}
 }

d43a81f49a466d844cce2a9e747fb4a6947ce80c
Alexandra Polunina <alex.polunina@gmail.com>
2017-04-18 12:50:38 +0300

    Класс IdOrGenerate записывает в recid существующий UUID, а в случае его отсутствия - генерирует новый

---
+++ v3/src/main/java/ownradio/util/IdOrGenerate.java
@@ -0,0 +1,29 @@
+package ownradio.util;
+
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SessionImplementor;
+import org.hibernate.id.UUIDGenerator;
+import ownradio.domain.AbstractEntity;
+
+import java.util.UUID;
+
+/**
+ * Created by a.polunina on 17.04.2017.
+ */
+
+	public class IdOrGenerate extends UUIDGenerator {
+		//private static final Logger log = Logger.getLogger(UseIdOrGenerate.class.getName());
+
+		@Override
+		public UUID generate(SessionImplementor session, Object obj) throws HibernateException {
+			if (obj == null) throw new HibernateException(new NullPointerException());
+
+			if (((AbstractEntity) obj).getRecid() == null) {
+				UUID id = UUID.randomUUID(); // super.generate(session, obj);
+				return id;
+			} else {
+				return ((AbstractEntity) obj).getRecid();
+
+			}
+		}
+	}

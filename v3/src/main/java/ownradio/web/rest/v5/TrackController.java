7d63279311ba5b58a591e1245733d379627a95d9
Alexandra Polunina <alex.polunina@gmail.com>
2017-06-27 13:40:18 +0300

    Задача #3191 Корректно обрабатывать попытку загрузить трек с guid, уже существующим на сервере

---
+++ v3/src/main/java/ownradio/web/rest/v5/TrackController.java
@@ -0,0 +1,91 @@
+package ownradio.web.rest.v5;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import ownradio.domain.Device;
+import ownradio.domain.Log;
+import ownradio.domain.Track;
+import ownradio.service.LogService;
+import ownradio.service.TrackService;
+
+import java.util.UUID;
+
+/**
+ * Created by a.polunina on 27.06.2017.
+ */
+@Slf4j
+//@CrossOrigin
+@RestController("TrackControllerV5")
+@RequestMapping(value = "/v5/tracks")
+public class TrackController {
+	private final TrackService trackService;
+	private final LogService logService;
+
+	public TrackController(TrackService trackService, LogService logService) {
+		this.trackService = trackService;
+		this.logService = logService;
+	}
+
+
+	@Data
+	private static class TrackDTO {
+		private UUID fileGuid;
+		private String fileName;
+		private String filePath;
+		private UUID deviceId;
+		private MultipartFile musicFile;
+
+		public Track getTrack() {
+			Device device = new Device();
+			device.setRecid(deviceId);
+
+			Track track = new Track();
+			track.setRecid(fileGuid);
+			track.setDevice(device);
+			track.setPath("---");
+			track.setLocaldevicepathupload(filePath);
+
+			return track;
+		}
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<?> save(TrackController.TrackDTO trackDTO) {
+		Log logRec = new Log();
+		logRec.setRecname("Upload");
+		logRec.setDeviceid(trackDTO.getDeviceId());
+		logRec.setLogtext("/v5/tracks; Body: TrackidId=" + trackDTO.getFileGuid() +", fileName=" + trackDTO.getFileName() + ", filePath=" + trackDTO.getFilePath() + ", deviceid=" + trackDTO.getDeviceId() + ", musicFile=" + (trackDTO.getMusicFile() != null ? trackDTO.getMusicFile().getOriginalFilename() : null));
+		logService.save(logRec);
+		if(trackService.getById(trackDTO.getTrack().getRecid()) != null){
+			logRec.setResponse("Http.Status=" + HttpStatus.CONFLICT + "; This uuid already exist!");
+			logService.save(logRec);
+			return  new ResponseEntity<>(HttpStatus.CONFLICT);
+		}
+
+		if (trackDTO.getMusicFile() == null || trackDTO.getMusicFile().isEmpty()) {
+			logRec.setResponse("Http.Status=" + HttpStatus.BAD_REQUEST + "; File is absent");
+			logService.save(logRec);
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+
+		try {
+			trackService.save(trackDTO.getTrack(), trackDTO.getMusicFile());
+			trackService.setTrackInfo(trackDTO.getTrack().getRecid());
+			logRec.setResponse("Http.Status=" + HttpStatus.CREATED);
+			logService.save(logRec);
+			return new ResponseEntity<>(HttpStatus.CREATED);
+		} catch (Exception e) {
+			logRec.setResponse("Http.Status=" + HttpStatus.INTERNAL_SERVER_ERROR + "; Error=" + e.getMessage());
+			logService.save(logRec);
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+	}
+}

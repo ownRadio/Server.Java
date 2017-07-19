ef39437cf61db291f7224f9a949af598bc26455e
Alexandra Polunina <alex.polunina@gmail.com>
2017-07-19 09:42:29 +0300

    разрешение конфликта при пулл реквесте

--- dbScripts/ownradio_db_v3.sql
+++ dbScripts/ownradio_db_v3.sql
@@ -871,16 +871,14 @@ BEGIN
                                                i_userid,
                                                'New user recname',
                                                now()
-	WHERE NOT EXISTS(SELECT recid FROM users WHERE recid = i_userid)
-	ON CONFLICT (recid) DO NOTHING;
+	WHERE NOT EXISTS(SELECT recid FROM users WHERE recid = i_userid);
 
     -- Добавляем новое устройство
     INSERT INTO devices (recid, userid, recname, reccreated) SELECT
                                                           i_deviceid,
                                                           i_userid,
                                                           'New device recname',
-                                                          now()
-	ON CONFLICT (recid) DO NOTHING;
+                                                          now();
   ELSE
     SELECT (SELECT userid
      FROM devices
@@ -1270,296 +1268,64 @@ $BODY$
 ALTER FUNCTION getnexttrackid_v7(uuid)
   OWNER TO "postgres";
 
- -- Function: public.getnexttrackid_v17(uuid)
 
--- DROP FUNCTION public.getnexttrackid_v17(uuid);
+-- Function: public.getrecommendedtrackid_v1(uuid)
 
-CREATE OR REPLACE FUNCTION public.getnexttrackid_v17(IN i_deviceid uuid)
-  RETURNS TABLE(track uuid, methodid integer, useridrecommended uuid, txtrecommendedinfo character varying) AS
+-- DROP FUNCTION public.getrecommendedtrackid_v1(uuid);
+
+CREATE OR REPLACE FUNCTION public.getrecommendedtrackid_v1(in_userid uuid)
+  RETURNS uuid AS
 $BODY$
 
--- Функция выдачи треков пользователю
 DECLARE
-	i_userid   UUID = i_deviceid; -- На случай, если устройство еще не зарегистрировано и пользователь не существует
-	rnd        INTEGER = (SELECT trunc(random() * 1001)); -- генерируем случайное целое число в диапазоне от 1 до 1000
-	o_methodid INTEGER; -- id метода выбора трека
-	owntracks  INTEGER; -- количество "своих" треков пользователя (обрезаем на 900 шт)
-	arrusers uuid ARRAY; -- массив пользователей для i_userid с неотрицательнымм коэффициентами схожести интересов
-	exceptusers uuid ARRAY; -- массив пользователей для i_userid с котороми не было пересечений по трекам
-	temp_trackid uuid; 
-	tmp_txtrecommendinfo text;
+preferenced_track uuid;
+
 BEGIN
-	-- temp_track - временная таблица для промежуточного результата (понадобилась чтобы найденные данные сначала сохранять в таблицу downloadtracks, а потом возвращать
-	DROP TABLE IF EXISTS temp_track; 
-	CREATE TEMP TABLE temp_track(track uuid, methodid integer, useridrecommended uuid, txtrecommendedinfo character varying);
-
-	--Если устройство не было зарегистрировано ранее - регистрируем его
-	IF NOT EXISTS(SELECT recid
-		  FROM devices
-		  WHERE recid = i_deviceid)
-	THEN
-
-		-- Добавляем нового пользователя
-		INSERT INTO users (recid, recname, reccreated) SELECT
-						   i_userid,
-						   'New user recname',
-						   now()
-		WHERE NOT EXISTS(SELECT recid FROM users WHERE recid = i_userid);
-
-		-- Добавляем новое устройство
-		INSERT INTO devices (recid, userid, recname, reccreated) SELECT
-							 i_deviceid,
-							 i_userid,
-							 'New device recname',
-							 now();
-	ELSE
-	-- Если устройство зарегистрировано - ищем соответствующего ему пользователя
-		SELECT (SELECT userid
-				FROM devices
-				WHERE recid = i_deviceid
-				LIMIT 1)
-		INTO i_userid;
-	END IF;
-
-
-	-- Выбираем следующий трек
-
-	-- Определяем количество "своих" треков пользователя
-	owntracks = (SELECT COUNT(*)
-				FROM ratings
-					WHERE userid = i_userid
-						AND ratingsum >= 0);
-
-	-- Если количество "своих" треков = 0 - выполняем процедуру предрекомендации
-	IF (owntracks = 0) THEN
-		o_methodid = 8; -- метод выбора из рекомендованных треков
-		SELECT o_trackid, o_textinfo INTO temp_trackid, tmp_txtrecommendinfo FROM populartracksrecommend_v1(i_userid);
-		-- Если такой трек найден - запись информацию о нем в downloadtracks, выход из функции, возврат найденного значения
-		IF temp_trackid IS NOT null THEN
-			INSERT INTO downloadtracks (SELECT uuid_generate_v4(),
-							now(),
-							null,
-							null, 
-							i_deviceid,
-							temp_trackid,
-							o_methodid,
-							(SELECT CAST((
-								tmp_txtrecommendinfo
-								) AS CHARACTER VARYING)),
-							(SELECT CAST((null) AS UUID)) );
-		RETURN QUERY 
-			SELECT temp_trackid,
-			o_methodid,
-			(SELECT CAST((null) AS UUID)),
-			(SELECT CAST((
-				tmp_txtrecommendinfo
-				) AS CHARACTER VARYING));
-		RETURN;
-		END IF;
-	END IF;
--- 	IF (rnd < owntracks)
--- 	THEN
--- 		o_methodid = 2; -- метод выбора из своих треков
--- 		INSERT INTO temp_track (
--- 		SELECT
--- 			trackid, -- выбираем id трека
--- 			o_methodid,
--- 			(SELECT CAST((null) AS UUID)),
--- 			(SELECT CAST(('случайный трек из своих') AS CHARACTER VARYING))
--- 		FROM ratings -- из треков, имеющих рейтинг для данного пользователя
--- 		WHERE userid = i_userid
--- 			  AND lastlisten < localtimestamp - INTERVAL '1 day' -- для которого последнее прослушивание было ранее, чем за сутки до выдачи
--- 			  AND ratingsum >= 0 -- рейтинг трека неотрицательный
--- 			  AND (SELECT isexist
--- 				   FROM tracks
--- 				   WHERE recid = trackid) = 1 -- трек существует на сервере
--- 			  AND ((SELECT length
--- 					FROM tracks
--- 					WHERE recid = trackid) >= 120 -- продолжительность трека больше двух минут
--- 				   OR (SELECT length
--- 					   FROM tracks
--- 					   WHERE recid = trackid) IS NULL) -- или длина трека не известна
--- 			  AND ((SELECT iscensorial
--- 					FROM tracks
--- 					WHERE recid = trackid) IS NULL -- трек должен быть цензурный или непроверенный
--- 				   OR (SELECT iscensorial
--- 					   FROM tracks
--- 					   WHERE recid = trackid) != 0)
--- 			  AND trackid NOT IN (SELECT trackid
--- 								  FROM downloadtracks
--- 								  WHERE reccreated > localtimestamp - INTERVAL '1 week' AND deviceid = i_deviceid) -- трек недолжен быть выдан в последнюю неделю
--- 		ORDER BY RANDOM()
--- 		LIMIT 1);
-
--- 		-- Если такой трек найден - запись информацию о нем в downloadtracks, выход из функции, возврат найденного значения
--- 		IF FOUND THEN
--- 			INSERT INTO downloadtracks (SELECT uuid_generate_v4(),now(),null, null, i_userid, temp_track.track AS trackid, temp_track.methodid AS methodid, temp_track.txtrecommendedinfo AS txtrecommendinfo, temp_track.useridrecommended AS userrecommend FROM temp_track);
--- 			RETURN QUERY SELECT * FROM temp_track;
--- 			RETURN;
--- 		END IF;
--- 	END IF;
-
-	-- Если rnd больше количества "своих" треков - используем алгоритм рекоммендаций
-
-	-- Если положительный коэффициент схожести интересов больше чем с пятью пользователями,
--- 	IF (SELECT COUNT (*) FROM ratios WHERE (userid1 = i_userid OR userid2 = i_userid) AND ratio >=0) > 5 THEN
-	-- рекомендуем трек с максимальным рейтингом среди пользователей, с которыми были пересечения
-		o_methodid = 7; -- метод выбора из рекомендованных треков
-		SELECT rn_trackid, rn_txtrecommendinfo INTO temp_trackid, tmp_txtrecommendinfo FROM getrecommendedtrackid_v5(i_userid);
-		-- Если такой трек найден - запись информацию о нем в downloadtracks, выход из функции, возврат найденного значения
-		IF temp_trackid IS NOT null THEN
-			INSERT INTO downloadtracks (SELECT uuid_generate_v4(),
-							now(),
-							null,
-							null, 
-							i_deviceid,
-							temp_trackid,
-							o_methodid,
-							(SELECT CAST((
-								tmp_txtrecommendinfo
-								) AS CHARACTER VARYING)),
-							(SELECT CAST((null) AS UUID)) );
-		RETURN QUERY 
-			SELECT temp_trackid,
-			o_methodid,
-			(SELECT CAST((null) AS UUID)),
-			(SELECT CAST((
-				tmp_txtrecommendinfo
-				) AS CHARACTER VARYING));
-		RETURN;
-		END IF;
--- 	END IF;
-
-	-- Если таких треков нет - выбираем популярный трек из ни разу не прослушанных пользователем треков
-	o_methodid = 3; -- метод выбора популярных из непрослушанных треков
-	INSERT INTO temp_track (
-	SELECT
-		trackid,
-		o_methodid,
-		(SELECT CAST((null) AS UUID)),
-		(SELECT CAST(('популярный трек из непрослушанных пользователем') AS CHARACTER VARYING))
-		FROM ratings
-			WHERE userid IN (SELECT recid FROM users WHERE experience >= 10)
-				AND userid != i_userid
-				AND (SELECT recid FROM tracks 
-						WHERE recid = trackid
-							AND isexist = 1 -- трек существует на сервере
-							AND (length >= 120 OR length IS NULL) -- продолжительность трека больше двух минут или длина трека не известна
-							AND (iscensorial != 0 OR iscensorial IS NULL)) IS NOT NULL --трек должен быть цензурный или непроверенный
-				AND trackid NOT IN (SELECT trackid
-							FROM downloadtracks
-							WHERE deviceid = i_deviceid)
-
-
-		GROUP BY trackid
-		ORDER BY sum(ratingsum) DESC, RANDOM()
-		LIMIT 1);
-
-	-- Если такой трек найден - запись информацию о нем в downloadtracks, выход из функции, возврат найденного значения
-	IF FOUND THEN
-		INSERT INTO downloadtracks (SELECT uuid_generate_v4(),now(),null, null, i_userid, temp_track.track AS trackid, temp_track.methodid AS methodid, temp_track.txtrecommendedinfo AS txtrecommendinfo, temp_track.useridrecommended AS userrecommend FROM temp_track);
-		RETURN QUERY SELECT * FROM temp_track;
-		RETURN;
-	END IF;
-
-	-- Если предыдущие запросы вернули null, выбираем случайный трек
-	o_methodid = 1; -- метод выбора случайного трека
-	INSERT INTO temp_track (
-	SELECT
-		recid,
-		o_methodid,
-		(SELECT CAST((null) AS UUID)),
-		(SELECT CAST(('случайный трек из всех') AS CHARACTER VARYING))
-	FROM tracks
-	WHERE isexist = 1 -- существующий на сервере 
-		  AND (iscensorial IS NULL OR iscensorial != 0) -- цензурный
-		  AND (length > 120 OR length IS NULL) -- продолжительностью более 2х минут
-	ORDER BY RANDOM()
-	LIMIT 1);
-	INSERT INTO downloadtracks (SELECT uuid_generate_v4(),now(),null, null, i_userid, temp_track.track AS trackid, temp_track.methodid AS methodid, temp_track.txtrecommendedinfo AS txtrecommendinfo, temp_track.useridrecommended AS userrecommend FROM temp_track);
-	RETURN QUERY SELECT * FROM temp_track;
-	RETURN;
+	-- Соединяем таблицу tracks с таблицой сумм произведений рейтинга трека на коэффициент
+	-- у конкретного пользователя для возможности вывода дополнительной информации о треке
+	-- в отладочных целях и для фильтра по столбцам tracks
+  	SELECT tracks.recid INTO preferenced_track
+  	--tracks.recid, table2.sum_rate, tracks.localdevicepathupload, tracks.path
+				FROM tracks
+				INNER JOIN (
+					--Группируем по треку и считаем сумму произведений рейтингов на коэффициент для
+					--каждого из них
+					SELECT trackid, SUM(track_rating) AS sum_rate
+					FROM(
+						--Запрашиваем таблицу с рейтингом всех треков, оцененных пользователями, которые имеют коэффициент
+						--с исходным, умноженным на их коэффициент
+						SELECT ratings.trackid, ratings.ratingsum * ratios.ratio AS track_rating, ratings.userid, ratios.ratio
+						FROM ratings
+							INNER JOIN ratios
+							--Выбираем рейтинги треков у тех пользователей, у которых есть пересечение
+							--с исходным в таблице ratios (кэффициенты совпадения вкусов), проверяя сначала
+							--с левой стороны
+							ON ((ratings.userid = ratios.userid2 AND ratios.userid1 = in_userid)
+								-- потом с правой
+								OR (ratings.userid = ratios.userid1 AND ratios.userid2 = in_userid))
+							AND ratings.userid <> in_userid --Выбирем все оценки треков, кроме оценок, данных исходным пользователем
+							AND ratios.ratio > 0 --Считать рейтинги треков, только у пользователей с положительным коэффициентом совпадения вкусов с исходным
+					) AS TracksRatings
+					GROUP BY trackid
+					ORDER BY sum_rate DESC
+				) AS table2
+				ON tracks.recid = table2.trackid
+				AND tracks.isexist = 1 --Трек должен существовать на сервере
+				AND tracks.iscensorial <> 0 --Трек не должен быть помечен как нецензурный
+				AND tracks.length >= 120
+				--Трек не должен был выдаваться исходному пользователю в течении последних двух месяцев
+				AND tracks.recid NOT IN (SELECT trackid FROM downloadtracks
+ 						         WHERE reccreated > localtimestamp - INTERVAL '2 months' AND deviceid = in_userid)
+				AND sum_rate > 0 --В итоге рекомендоваться будут только треки с положительной суммой произведений рейтингов на коэффициенты
+				ORDER BY table2.sum_rate DESC
+					 --Сортировка по второму столбцу нужна для случаев, когда получаем много треков с одинковым table2.sum_rate,
+					 --в таких случаях план выполнения запроса меняется и производительность сильно падает
+					 ,tracks.recid
+				LIMIT 1;
+	RETURN preferenced_track;
 END;
-$BODY$
-  LANGUAGE plpgsql VOLATILE
-  COST 100
-  ROWS 1000;
-ALTER FUNCTION public.getnexttrackid_v17(uuid)
-  OWNER TO "a.polunina";
-
-
-  -- Function: public.getnexttrack_v2(uuid)
-
--- DROP FUNCTION public.getnexttrack_v2(uuid);
-
-CREATE OR REPLACE FUNCTION public.getnexttrack_v2(IN i_deviceid uuid)
-  RETURNS TABLE(track character varying, method integer, useridrecommended character varying, txtrecommendedinfo character varying, timeexecute character varying) AS
-$BODY$
-DECLARE
-		declare t timestamptz := clock_timestamp(); -- запоминаем начальное время выполнения процедуры
-		i_userid UUID = i_deviceid; -- в дальнейшем заменить получением userid по deviceid
-BEGIN
-	-- Добавляем устройство, если его еще не существует
-	PERFORM registerdevice(i_deviceid, 'New device');
-
-	-- Возвращаем trackid, конвертируя его в character varying, и methodid
-	RETURN QUERY SELECT
-					 CAST((nexttrack.track) AS CHARACTER VARYING),
-					 nexttrack.methodid,
-					 CAST((nexttrack.useridrecommended) AS CHARACTER VARYING),
-					 nexttrack.txtrecommendedinfo,
-					 CAST((clock_timestamp() - t ) AS CHARACTER VARYING) -- возвращаем время выполнения процедуры
-				 FROM getnexttrackid_v17(i_deviceid) AS nexttrack;
-END;
-$BODY$
-  LANGUAGE plpgsql VOLATILE
-  COST 100
-  ROWS 1000;
-ALTER FUNCTION public.getnexttrack_v2(uuid)
-  OWNER TO postgres;
-
-  
-  -- Function: public.populartracksrecommend_v1(uuid)
-
--- DROP FUNCTION public.populartracksrecommend_v1(uuid);
-
-CREATE OR REPLACE FUNCTION public.populartracksrecommend_v1(
-    IN i_userid uuid,
-    OUT o_trackid uuid,
-    OUT o_textinfo character varying)
-  RETURNS record AS
-$BODY$
-
--- Функция выдачи треков пользователю, не имеющему пользователей со схожим вкусом
-BEGIN
-
-	WITH exclude_users AS (
-		SELECT r.userid 
-			FROM downloadtracks d
-				INNER JOIN ratings r
-					ON d.trackid = r.trackid
-			WHERE d.deviceid = i_userid
-				AND r.userid IN (SELECT recid FROM users WHERE experience >= 10)
-			GROUP BY r.userid)
-	SELECT recid, 'предрекомендация, суммарный рейтинг трека ' || rate INTO o_trackid, o_textinfo 
-		FROM (
-		SELECT t.recid, SUM(r.ratingsum) AS rate
-			FROM tracks t
-				INNER JOIN ratings r
-					ON t.recid = r.trackid    
-						AND r.userid IN (SELECT recid FROM users WHERE experience >= 10)
-			WHERE t.recid NOT IN (SELECT trackid FROM ratings WHERE userid IN (SELECT * FROM exclude_users) GROUP BY trackid)
-				AND isexist = 1
-				AND (iscensorial IS NULL OR iscensorial != 0)
-				AND (length > 120 OR length IS NULL)
-			GROUP BY t.recid
-			ORDER BY rate DESC) AS res
-		WHERE rate > 0
-		LIMIT 1;
-END;
-
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-ALTER FUNCTION public.populartracksrecommend_v1(uuid)
+ALTER FUNCTION public.getrecommendedtrackid_v1(uuid)
   OWNER TO postgres;
